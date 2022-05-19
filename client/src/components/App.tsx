7fa8af0ce9c88de1b6a77fee2eeefa119cbbcd2b
Patrick Liang <pl204511194@yahoo.com>
2022-05-19 13:54:18 -0400

    ts react setup

---
+++ client/src/components/App.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    async function handleFetch() {
+      const helloData = await axios.get("/hello");
+      setCount(helloData.data.count);
+    }
+    handleFetch();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Page Count: {count}</h1>
+    </div>
+  );
+}
+
+export default App;
