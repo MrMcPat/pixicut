315cff936fd2c5a4d95a53d6685546cbed1c9bb2
Patrick Liang <pl204511194@yahoo.com>
2022-05-21 09:25:18 -0400

    grid added

--- client/src/components/canvas/Canvas.js
+++ client/src/components/canvas/Canvas.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Canvas.css";
 import Tile from "./Tile";
 
 function Canvas() {
+  const [tiles, setTiles] = useState([]);
+
+  useEffect(() => {
+    let array = [];
+    for (let i = 0; i < 64; i++) {
+      array.push(i);
+    }
+    setTiles(array);
+  }, []);
+
+  console.log(tiles);
+
   return (
     <div>
       <div className="eightbyeight-container">
-        <Tile />
+        {tiles.map((tile) => (
+          <Tile key={tile} />
+        ))}
       </div>
     </div>
   );
